0d77f0ffe51855fff6268f7b1bce5f06cbeb01ae
lapshuntanya <lapshun.tatiana@gmail.com>
2025-05-12 18:54:25 +0300

    main

--- P44-T14-Violations.cpp
+++ P44-T14-Violations.cpp
@@ -21,8 +21,8 @@ int main()
 
     cout << "=============================\n";
     a.printViol();
-    b.printViol(); */
-
+    b.printViol(); 
+    cout << "=============================\n";
     Driver a{ "Pupkin Anton Petrovich", "AX1234AX", 2, new Violation[2]{
         {{1, 1, 2000}, "Speed 200 km/h", 9999},
         {{12, 5, 2025}, "Parking", 1000}
@@ -33,7 +33,24 @@ int main()
     cout << "\n\n\n";
 
     a.printDriver();
-    b.printDriver();
+    b.printDriver();*/
+
+    int NDrivers = 3;       //Кількіть водіїв в базі = Розмір масиву
+    Driver* arrDrivers = new Driver[3]{
+        { "Pupkin Anton Petrovych", "AX1234AX", 2, new Violation[2]{
+            {{1, 1, 2000}, "Speed 200 km/h", 9999},
+            {{12, 5, 2025}, "Parking", 1000}
+        } },
+        {
+            "Shevchenko Mykola Olehovych", "BI7777IO",1,  new Violation[1]{
+                {{11, 5, 2025}, "Driving under the influence of alcohol", 25000}
+         } },
+        {
+            "Sumkina Kateryna Victorivna", "AX7654CT", 1, new Violation[1]{
+                {{11, 5, 2025}, "Parking", 600}
+        } }
+    };
+
 
 }
 
