f77533a6774b94e1aa34124a3a60768b82095539
lapshuntanya <lapshun.tatiana@gmail.com>
2025-05-12 17:30:07 +0300

    Add project files.

---
+++ P44-T14-Violations.cpp
@@ -0,0 +1,18 @@
+#include "ArrayTemplate.h"
+#include "Date.h"
+
+int main()
+{
+    //Date myDay = { 1, 1, 2000 };
+    //Date now; // 0 0 0
+    //now.getNowDate(); // 12 05 2025
+    //myDay.printDate();
+    //now.printDate();
+    //cout << "============================\n";
+    //cout << myDay.toString() << endl;
+    //cout << myDay.toString("%Y %m %d") << endl;
+
+
+
+}
+
---
+++ Violation.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Date.h"
+
+struct Violation
+{
+	//Властивості
+	Date dateViol;			//Дата порушення
+	char info[100] = "";	//Опис порушення
+	float fine = 0;			//Сума штрафу
+
+	//Методи
+
+
+};
