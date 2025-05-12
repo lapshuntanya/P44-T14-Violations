4f1e24c14a542bc980e9d28a8fd25e521240507e
lapshuntanya <lapshun.tatiana@gmail.com>
2025-05-12 18:18:25 +0300

    struct  Driver

--- Driver.h
+++ Driver.h
@@ -11,7 +11,7 @@ struct  Driver
 	void fillDriver() {
 		cout << "Input name: ";
 		cin.getline(name, 100);
-		cout << "Input number of car (AX1111AX): ";
+		cout << "Input number of car (AX0000AX): ";
 		cin >> carNumber;
 		sizeViol = 1;
 		arrViol = new Violation[1];
@@ -19,4 +19,16 @@ struct  Driver
 		arrViol[0].fillViol();
 	}
 
+	void printDriver() {
+		cout << "==================================\n";
+		cout << "Driver: " << name << endl;
+		cout << "Car:    " << carNumber << endl;
+		cout << "Amount of viol: " << sizeViol << endl;
+		for (int i = 0; i < sizeViol; i++)
+		{
+			cout << i + 1 << " ";
+			arrViol[i].printViol();
+		}
+		cout << "==================================\n";
+	}
 };
