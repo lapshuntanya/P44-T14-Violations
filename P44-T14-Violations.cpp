39ae437911429a6b566fbabc578e06bdd225d68b
lapshuntanya <lapshun.tatiana@gmail.com>
2025-05-12 19:11:42 +0300

    final main 2.0

--- P44-T14-Violations.cpp
+++ P44-T14-Violations.cpp
@@ -21,7 +21,56 @@ int main()
                 {{11, 5, 2025}, "Parking", 600}
         } }
     };
+    //============================================================================
 
+    int menu = 0;
+    Driver tmpD;
+
+    do
+    {
+        cout << "======================================\n";
+        cout << "\t MENU:\n";
+        cout << "======================================\n";
+        cout << "\t 0.Exit\n";
+        cout << "\t 1.List of drivers\n";
+        cout << "\t 2.Add a new driver\n";
+        cout << "\t 3.Find driver by number of car\n";
+        cout << "======================================\n";
+        cout << ">>>";
+        cin >> menu;
+
+        switch (menu)
+        {
+        default: cout << "Error!!!\n"; break;
+        case 0: cout << "Good bye! =) \n"; break;
+        case 1:
+            if (NDrivers == 0) cout << "List is empty!\n"; break;
+            for (int i = 0; i < NDrivers; i++)
+            {
+                arrDrivers[i].printDriver();
+            }
+            break;
+        case 2:
+            cin.ignore();
+            tmpD.fillDriver();
+            addItemBack(arrDrivers, NDrivers, tmpD);
+            break;
+        case 3:
+        {
+            char car[10] = "";
+            cout << "Input number of car (AX0000AX): "; cin >> car;
+            for (int i = 0; i < NDrivers; i++)
+            {
+                if (stricmp(arrDrivers[i].carNumber, car) == 0) {
+                    arrDrivers[i].printDriver();
+                    cout << "Final fine: " << arrDrivers[i].calcTotalFines() << endl;
+                    break;
+                }
+            }
+        }break;
+        }
+
+    } while (menu != 0);
 
 }
 
