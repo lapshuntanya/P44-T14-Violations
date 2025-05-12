b822ba3271fa07c1a8ab252d607fe3785cd51e59
lapshuntanya <lapshun.tatiana@gmail.com>
2025-05-12 18:31:43 +0300

    main

--- P44-T14-Violations.cpp
+++ P44-T14-Violations.cpp
@@ -1,6 +1,7 @@
 #include "ArrayTemplate.h"
 #include "Date.h"
 #include "Violation.h"
+#include "Driver.h"
 
 int main()
 {
@@ -13,14 +14,26 @@ int main()
     //cout << myDay.toString() << endl;
     //cout << myDay.toString("%Y %m %d") << endl;
 
-    Violation a{ {1, 1, 2000}, "Speed 200 km/h", 5000 };
+   /* Violation a{{1, 1, 2000}, "Speed 200 km/h", 5000};
     Violation b; // {0 0 0}, "", 0
 
     b.fillViol();
 
     cout << "=============================\n";
     a.printViol();
-    b.printViol();
+    b.printViol(); */
+
+    Driver a{ "Pupkin Anton Petrovich", "AX1234AX", 2, new Violation[2]{
+        {{1, 1, 2000}, "Speed 200 km/h", 9999},
+        {{12, 5, 2025}, "Parking", 1000}
+    } };
+
+    Driver b; // "", "", 0, nullptr
+    b.fillDriver();
+    cout << "\n\n\n";
+
+    a.printDriver();
+    b.printDriver();
 
 }
 
